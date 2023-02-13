7dd1d928234c6d9a78d75052e513163b9d674977
Yindy07 <125229498+Yindy07@users.noreply.github.com>
2023-02-13 15:19:20 +0900

    0213
    실습

---
+++ Exam2.java
@@ -0,0 +1,14 @@
+package output;
+
+import java.util.Scanner;
+
+public class Exam2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("age?");
+		sc.next();
+
+	}
+
+}
