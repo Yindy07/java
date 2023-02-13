fc5aa0365d557018a660cbc486919eb2edf6fc2c
Yindy07 <125229498+Yindy07@users.noreply.github.com>
2023-02-13 15:24:31 +0900

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,14 @@
+package test;
+//public, private, default : 접근제어자
+//static : 예약어 프로그램이 실행되면 같이 호출딤
+// void 반환자료형,(void, integer, string, double,..)
+// return : 반환 데이터 
+// String[] args : 변수(데이터 저장 공간),매개변수(매개체 변수)
+// 매개변수 : argument, parameter
+// ctrl+/ 드레그 주석
+public class Test1 {
+	public static void main(String[] args) {
+		
+	}
+	// public void te()-> 메인메서드에서 직접호출해야 실행
+}
