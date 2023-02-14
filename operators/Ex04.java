4d1a6d4eee15eb2618bda76345307579156d14d6
Yindy07 <125229498+Yindy07@users.noreply.github.com>
2023-02-14 15:49:39 +0900

    0214

---
+++ operators/Ex04.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		double do1 = 3.0, do2 = 3.1;
+		
+		boolean result = do1<=do2;
+		System.out.println("result = " + result);
+		
+		System.out.println("do1 <= do2 크기 비교 " + (do1 <= do2));
+		System.out.println("do1 >= do2 크기 비교 " + (do1 >= do2));
+		System.out.println("do1 == do2 크기 비교 " + (do1 == do2));
+		System.out.println("do1 != do2 크기 비교 " + (do1 != do2));
+	}
+
+}
