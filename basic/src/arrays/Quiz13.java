f2afbba4a2831804f598e9367450388f5f6836b7
Yindy07 <125229498+Yindy07@users.noreply.github.com>
2023-02-27 15:45:39 +0900

    Update Quiz13.java

--- basic/src/arrays/Quiz13.java
+++ basic/src/arrays/Quiz13.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		int grade, num1 = 0, num2=0;
+		int num1 = 0;
+		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		System.out.print("총 학급 수 : ");
 		num1 = sc.nextInt();		//총 학급 수
@@ -13,12 +14,21 @@ public class Helloworld {
 		String[][] name = new String[num1][];
 		int [][] kor = new int[num1][];
 		int [][] mt = new int[num1][];
+		int[][] total = new int[num1][];
+		double[][] avg = new double [num1][];
+		int rankC[][] = new int[num1][];
+		int rankA[][] = new int[num1][];
 		for(int i=0; i<cl.length; i++) {
 			System.out.print((i+1)+"반의 인원수 : ");
 			cl[i] = sc.nextInt();			//각 학급의 인원 수
 			name[i] = new String[cl[i]];
 			kor[i] = new int [cl[i]];
 			mt[i] = new int [cl[i]];
+			total[i]= new int[cl[i]];
+			avg[i] = new double[cl[i]];
+			rankC[i] = new int [cl[i]];
+			rankA[i] = new int [cl[i]];
+			
 		}
 		
 		
@@ -31,16 +41,29 @@ public class Helloworld {
 			kor[i][j] = sc.nextInt();
 			System.out.print("수학 : ");
 			mt[i][j] = sc.nextInt();
+			total[i][j] = mt[i][j]+kor[i][j];
+			avg[i][j] = (double)total[i][j]/2;
 			}
 		}
+		int[][] total2 = total;
+		for(int i=0; i<cl.length; i++)
+		{
+			for(int j=0; j<rankC[i].length; j++)
+			{
+				int tmp = total2[i][j];
+				total2[i][j] =  total2[i][j+1];
+				total2[i][j+1] = tmp;
+			}
+		}
+		
 		System.out.println("이름\t국어\t영어\t총점\t반석차\t전교석차\t평균");
 		for(int i=0; i<cl.length; i++)
 		{
-			for(int j=0; j<cl[i]; j++) {
-				System.out.println
-				(name[i][j] + "\t"+kor[i][j]+ "\t" + mt[i][j]+ "\t");
+			for(int j=0; j<name[i].length; j++) {
+				System.out.println(total2[i][j]);
+//				System.out.println
+//				(name[i][j] + "\t"+kor[i][j]+ "\t" + mt[i][j]+ "\t"+total[i][j] + "\t\t\t" + avg[i][j]);
 			}
 		}
 	}
-}
-
+} 
