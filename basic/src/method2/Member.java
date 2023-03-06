a443fabe2d1c35ce7f37775fec7251e6f684bf41
Yindy07 <125229498+Yindy07@users.noreply.github.com>
2023-03-06 21:08:24 +0900

    Update Member.java

--- basic/src/method2/Member.java
+++ basic/src/method2/Member.java
@@ -1,14 +1,14 @@
-package method2;
+package method02;
 
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Member {
 	Scanner sc = new Scanner(System.in);
-	private HashMap<String, String> passwords = new HashMap<>();
-	private HashMap<String, String> names = new HashMap<>();
+	private static HashMap<String, String> passwords = new HashMap<>();
+	private static HashMap<String, String> names = new HashMap<>();
 	
-	public void insert(String id, String pass, String name)
+	public static void insert(String id, String pass, String name)
 	{
 		if(passwords.containsKey(id) == false) {
 			passwords.put(id, pass);
@@ -16,22 +16,37 @@ public class Member {
 		}
 		else System.out.println(id + " 중복");
 	}
-	public void selectId() {
-		
+	public static void selectId(String id) {
+		if(passwords.containsKey(id) == false) {
+			System.out.println("등록되지 않은 아이디입니다.");
+		}else {
+			System.out.println("## 회원 검색 결과 ##");
+			System.out.println("아이디 : " + id);
+			System.out.println("비밀번호 : " + passwords.get(id));
+			System.out.println("이름 : " + names.get(id));
+		}
+		System.out.println();
 	}
-	public void delete(String id) {
-		if(passwords.containsKey(id)) {
-			passwords.remove(id);
+	public static void delete(String id) {
+		if(passwords.containsKey(id) == false) {
+			System.out.println("등록되지 않은 아이디입니다.");
+		}else {
 			names.remove(id);
+			passwords.remove(id);
+			System.out.println(id + " 계정 정보 삭제 완료");
+			System.out.println();
 		}
-		else System.out.println("등록된 id가 아닙니다.");
-		}
-	public void selectAll() {
-		
-		for(int i=0; i<passwords.size(); i++)
-			System.out.println(passwords.keySet()+"\t"+passwords.get(i)+"\t"+names.get(i));
 	}
-	public void update(String id) {
+	public static void selectAll() {
+		
+		for(String key : names.keySet()) {
+			System.out.println("아이디 : " + key);
+			System.out.println("비밀번호 : " + passwords.get(key));
+			System.out.println("이름 : " + names.get(key));
+		}
+	}
+	public static void update(String id) {
+		Scanner sc = new Scanner(System.in);
 		if(passwords.containsKey(id)) {
 			System.out.print("변경할 비밀번호 : ");
 			String pass = sc.next();
@@ -42,4 +57,4 @@ public class Member {
 		}
 		else System.out.println("등록된 id가 아닙니다.");
 		}
-	}
+}
